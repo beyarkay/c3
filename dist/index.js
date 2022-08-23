8f368c1bbc2aeaa4a7a048154c556c519dd00299
beyarkay <boydrkane@gmail.com>
2022-08-23 21:58:07 +0200

    Set outputs and increase logging

--- dist/index.js
+++ dist/index.js
@@ -18603,7 +18603,9 @@ async function run() {
     const paths = await globber.glob()
 
     core.info(`Paths are ${paths}`);
+    let ics_paths = [];
     for (const path of paths) {
+      console.log(`::group::Path ${path}`)
       core.info(`Reading in path: ${path}`);
       const doc = yaml.load(fs.readFileSync(path, 'utf8'));
       if (!doc.hasOwnProperty('events')) {
@@ -18615,6 +18617,7 @@ async function run() {
       core.info(`Iterating over the ${doc['events'].length} event(s) found in ${path}`);
       events = [];
       for (const event of doc['events']) {
+        console.log(`::group::Event ${event['title']}`);
         core.info(`Parsing event: ${JSON.stringify(event)}`);
         if (!(event['start'] instanceof Date)) {
           core.setFailed(`FAIL: event['start'] is '${event['start']} which is not a date.'`);
@@ -18626,11 +18629,11 @@ async function run() {
           start: isoStringToDateList(event['start'].toISOString()),
           end: isoStringToDateList(event['end'].toISOString()),
         });
+        console.log(`::endgroup::`);
       }
 
       core.info(`Creating calendar from events found in ${path}: ${JSON.stringify(events)}`);
       const { error, value } = ics.createEvents(events);
-      core.info(`Error: ${JSON.stringify(error)}, calendar: ${JSON.stringify(value)}`);
       if (error) {
         core.setFailed(`FAIL: Couldn't create .ics file from events found in ${path}. Error message from dependency [ics](https://www.npmjs.com/package/ics): ${JSON.stringify(error)}`);
         return
@@ -18639,7 +18642,11 @@ async function run() {
       core.info(`Saving calendar to ${ics_path}:\n${value}`);
       fs.writeFileSync(ics_path, value);
       core.info(`Wrote calendar to ${ics_path}`);
+      ics_paths.push(ics_path);
+      console.log("::endgroup::");
     }
+    let ics_paths_str = ics_paths.join(',')
+    console.log('::set-output name=file-paths::ics_paths_str');
     core.info(`Ending script at ${(new Date()).toTimeString()}`);
   } catch (error) {
     core.setFailed(error.message);
